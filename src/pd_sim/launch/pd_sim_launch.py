570f1acb3da8159dc46c9bf50d30b6232d20cad9
Dmitrii Drozdov <da.drozdov85@gmail.com>
2024-11-19 00:30:00 +0100

    Eliminate hard-coded path in pd_sim_launch.py

--- src/pd_sim/launch/pd_sim_launch.py
+++ src/pd_sim/launch/pd_sim_launch.py
@@ -1,8 +1,15 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'robot_model',
+            default_value='models/robot_simple.gltf',
+            description='Path to the robot model'
+        ),
         Node(
             package="pd_sim",
             executable="pd_sim_node",
@@ -10,7 +17,7 @@ def generate_launch_description():
             output="screen",
             emulate_tty=True,
             parameters=[
-                {"robot_model": "/home/dadrozdov/repos/projective-dynamics-for-robotics/models/robot_simple.gltf"}
+                {"robot_model": LaunchConfiguration('robot_model')}
             ]
         )
     ])
